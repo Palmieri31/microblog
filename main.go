e24080118459c081423e7a9100681cc94b65b5f5
Federico Palmieri <fedepalmie95@gmail.com>
2021-02-10 00:05:12 -0300

    db and handlers added

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+    "log"
+    "github.com/Palmieri31/microblog/handlers"
+    "github.com/Palmieri31/microblog/bd"
+)
+
+
 func main(){
-	
+	if bd.ChequeoConnection() == 0 {
+        log.Fatal("Sin conexión a la BD")
+        return
+    }
+    handlers.Manejadores()
+
 }
