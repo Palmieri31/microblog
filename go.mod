c873150d5bf639e51d549ce26a9e81c44708b9c2
Federico Palmieri <fedepalmie95@gmail.com>
2021-02-14 17:51:11 -0300

    commit del registro de usuarios

--- go.mod
+++ go.mod
@@ -5,7 +5,8 @@ go 1.13
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/gorilla/mux v1.8.0
+	github.com/lutrueba/microblog v0.0.0-20210202154723-4c9af1b0bf50
 	github.com/rs/cors v1.7.0
 	go.mongodb.org/mongo-driver v1.4.6
-	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
+	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad
 )
--- handlers/handlers.go
+++ handlers/handlers.go
@@ -5,12 +5,18 @@ import (
     "net/http"
     "os"
 	"github.com/gorilla/mux"
+    "github.com/Palmieri31/microblog/middlew"
+    "github.com/Palmieri31/microblog/routers"
     "github.com/rs/cors"
 )
  
 /*Manejadores: seteo mi puerto, el handler y pongo a escuchar al servidor*/
 func Manejadores(){
     router := mux.NewRouter()
+
+    /*Por cada EndPoint vamos a tener un renglon de codigo que permita manejar la funcion correspondiente*/
+    router.HandleFunc("/registro", middlew.ChequeoBD(routers.Registro)).Methods("POST")
+
     PORT := os.Getenv("PORT")
     if PORT == ""{
         PORT = "8080"
